eb2ef785a81311948166e1540c1382fd7d7afc7c
Minjun Kim (Lapis0875) <lapis0875@kakao.com>
2021-06-08 00:45:58 +0900

    feat: Update test code.
    1. Finish implementing event-listener style serial handler (SerialProgram)
      Currently, tested with Arduino Uno. Successfully sending&receiving datas with server(python code) and client(Arduino).
    2. Investigating why Serial data is sent with non-unicode bytes '\xff'
      why this is included in data received from python server?

--- test/serial_test.py
+++ test/serial_test.py
@@ -12,12 +12,67 @@ BAUDRATE: Final[int] = 9600
 
 
 class SerialProgram:
+    class ServerData:
+        prefix: Final[str] = 's;'
+
+        def __init__(self, text: str, encoding: str):
+            self.text = text
+            self.encoding = encoding
+
+        def serialize(self):
+            return (self.prefix + self.text).encode(self.encoding)
+
+        def __repr__(self):
+            return f'ServerData({self.text}).encode({self.encoding})'
+
+    class ClientData:
+        prefix: Final[bytes] = b'c;'
+
+        @classmethod
+        def validate(cls, data: bytes):
+            return data.startswith(cls.prefix)
+
+        @classmethod
+        def deserialize(cls, data: bytes) -> SerialProgram.ClientData:
+            return cls(data)
+
+        def __init__(self, data: bytes):
+            self.data = data.strip(self.prefix)
+
+        def serialize(self) -> bytes:
+            return self.prefix + self.data
+
+        def force_encode(self, encoding: str) -> str:
+            try:
+                return self.data.decode(encoding)
+            except UnicodeDecodeError:
+                return self.data.replace(b'\xff', b'?').decode(encoding)
+
+        def __repr__(self):
+            return f'ClientData({self.data})'
+
     def __init__(self, serial_port: serial.Serial):
         self._serial: serial.Serial = serial_port
+        self._encoding: str = 'utf-8'
+        self._close_flag: bool = False
+
+        # listeners/handlers
         self.__byte_listeners__: List = []
         self.__line_listeners__: List = []
-        self._encoding: str = 'utf-8'
+        self.__data_listeners__: List = []
         self.__serial_input__ = None    # Callable function which receives serial.Serial object as a only parameter.
+
+    @property
+    def serial(self) -> serial.Serial:
+        return self._serial
+
+    @property
+    def port(self) -> str:
+        return self._serial.port
+
+    @property
+    def baudrate(self) -> int:
+        return self._serial.baudrate
 
     @property
     def encoding(self) -> str:
@@ -27,8 +82,27 @@ class SerialProgram:
     def encoding(self, new_encoding):
         self._encoding = new_encoding
 
+    def wrap_server_data(self, text: str) -> SerialProgram.ServerData:
+        return SerialProgram.ServerData(text, self.encoding)
+
+    def wrap_client_data(self, data: bytes) -> SerialProgram.ClientData:
+        return SerialProgram.ClientData.deserialize(data)
+
+    def schedule_close(self):
+        console_print(f'{self} > scheduling loop close.')
+        self._close_flag = True
+
     def loop(self):
+        console_print(f'{self} > starting loop...')
+        if not self._serial.is_open:
+            console_print(f'{self} > serial port is not open. try opening it...')
+            self._serial.open()
+            console_print(f'{self} > opened serial port.')
+        else:
+            console_print(f'{self} > serial port is already open.')
         while True:
+            if self._close_flag:
+                break
             buffer: bytes = bytes()
             console_print(f'{self} > dispatching serial input handler :')
             self.__serial_input__(self._serial)
@@ -42,17 +116,47 @@ class SerialProgram:
                 buffer += single_byte
                 if single_byte == serial.LF:
                     for listener in self.__line_listeners__:
-                        listener(buffer.decode(self._encoding))
+                        listener(buffer)
+                    if buffer.startswith(SerialProgram.ClientData.prefix):
+                        for listener in self.__data_listeners__:
+                            listener(self.wrap_client_data(buffer))
                     buffer = bytes()
 
+        # Close loop
+        console_print(f'{self} > closing loop.')
+        self._serial.close()
+        console_print(f'{self} > closed serial loop.')
+
+    def write(self, data: SerialProgram.ServerData):
+        self._serial.write(data.serialize())
+
     def byte_listener(self, func):
+        """
+        register listener which listens every single byte on serial.
+        :param func: function to register as byte-listener.
+        :return: original function.
+        """
         console_print(f'{self} > registering function {func} as serial byte listener.')
         self.__byte_listeners__.append(func)
         return func
 
     def line_listener(self, func):
+        """
+        register listener which listens single line data (bytes data which ends with \n).
+        :param func: function to register as line-listener.
+        :return: original function.
+        """
         console_print(f'{self} > registering function {func} as serial line listener.')
         self.__line_listeners__.append(func)
+
+    def data_listener(self, func):
+        """
+        register listener which listens wrapped python object containing single line data.
+        :param func: function to register as line-listener.
+        :return: original function.
+        """
+        console_print(f'{self} > registering function {func} as serial data listener.')
+        self.__data_listeners__.append(func)
 
     def serial_input(self, func):
         console_print(f'{self} > registering function {func} as serial input handler.')
@@ -99,8 +203,8 @@ def receive(serial: serial.Serial) -> SerialData:
     return SerialData.deserialize(data=line.strip().strip(b'\xba').decode('utf-8'))
 
 
-CONSOLE_PREFIX: Final[str] = 'Console :'
-console_print = partial(print, CONSOLE_PREFIX)
+CONSOLE_PREFIX: Final[str] = 'Console'
+console_print = partial(print, CONSOLE_PREFIX + ' :')
 
 
 def main():
@@ -140,31 +244,41 @@ def text_send_test(serial_port: serial.Serial):
 
 
 def serial_loop_test():
-    serial_port = serial.Serial("COM10", BAUDRATE)
+    serial_port = serial.Serial("COM12", BAUDRATE)
     serial_loop = SerialProgram(serial_port)
 
     @serial_loop.serial_input
     def serial_source(port: serial.Serial):
-        port.write(b'bytes! yeah!')
-        port.writelines(f'current system time : {time.time()}'.encode(serial_loop.encoding))
-        port.flushInput()
+        text: str = input(f'{CONSOLE_PREFIX} : Write any text to send in serial >')
+        if not text.endswith(LINE_SEP):
+            text += LINE_SEP
+        data: SerialProgram.ServerData = serial_loop.wrap_server_data(text)
+        console_print(f'Your input (utf-8 encoded) > {data.serialize()}')
+        serial_loop.write(data)
 
-    @serial_loop.byte_listener
-    def byte_example(single_byte: bytes):
-        console_print(byte_example.__name__, f'> I received a single byte! `{single_byte}`')
-
-    @serial_loop.byte_listener
-    def char_commands(single_byte: bytes):
-        if single_byte == b'y':
-            console_print(char_commands.__name__, f'> What do you mean yes, sir?')
-        elif single_byte == b'n':
-            console_print(char_commands.__name__, f'> What do you mean no, sir?')
-        elif single_byte == b't':
-            console_print(char_commands.__name__, f"> I'll bring you some teas, sir.")
+    # @serial_loop.byte_listener
+    # def byte_example(single_byte: bytes):
+    #     console_print(byte_example.__name__, f'> I received a single byte! `{single_byte}`')
+    #
+    # @serial_loop.byte_listener
+    # def char_commands(single_byte: bytes):
+    #     if single_byte == b'y':
+    #         console_print(char_commands.__name__, f'> What do you mean yes, sir?')
+    #     elif single_byte == b'n':
+    #         console_print(char_commands.__name__, f'> What do you mean no, sir?')
+    #     elif single_byte == b't':
+    #         console_print(char_commands.__name__, f"> I'll bring you some teas, sir.")
+    #     elif single_byte == b'q':
+    #         serial_loop.schedule_close()
 
     @serial_loop.line_listener
-    def line_example(line: str):
-        console_print(line_example.__name__, f'> I received a line! `{line}`')
+    def line_example(line: bytes):
+        print(f'Serial({serial_loop.port}) > {line}')
+
+    @serial_loop.data_listener
+    def data_example(data: SerialProgram.ClientData):
+        console_print(f'{serial_loop} > data > `{data.serialize()}`')
+        console_print(f'{serial_loop} > data > try force encode : {data.force_encode(serial_loop.encoding)}')
 
     serial_loop.loop()
 
